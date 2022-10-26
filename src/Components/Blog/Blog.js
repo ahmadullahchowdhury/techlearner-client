4a44575df9c5b5f87e61cd78991218ffb2571408
kowchergit config --global user.email kowcher99@gmail.com <37480985+kowcher99@users.noreply.github.com>
2022-10-27 01:33:05 +0600

    blog section updated

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -19,59 +19,48 @@ const Blog = () => {
             <AccordionItem>
                 <AccordionItemHeading>
                     <AccordionItemButton>
-                        What harsh truths do you prefer to ignore?
+                    what is cors?
                     </AccordionItemButton>
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                        Exercitation in fugiat est ut ad ea cupidatat ut in
-                        cupidatat occaecat ut occaecat consequat est minim minim
-                        esse tempor laborum consequat esse adipisicing eu
-                        reprehenderit enim.
+                    “CORS” stands for Cross-Origin Resource Sharing. It allows one server  to make requests from one another website in the browser, which is normally prohibited by another browser policy called the Same-Origin Policy (SOP).By default for security reasons, browsers restrict cross-origin HTTP requests that initiated from scripts.
                     </p>
                 </AccordionItemPanel>
             </AccordionItem>
             <AccordionItem>
                 <AccordionItemHeading>
                     <AccordionItemButton>
-                        What harsh truths do you prefer to ignore?
+                    Why are you using firebase? What other options do you have to implement authentication?
                     </AccordionItemButton>
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                        Exercitation in fugiat est ut ad ea cupidatat ut in
-                        cupidatat occaecat ut occaecat consequat est minim minim
-                        esse tempor laborum consequat esse adipisicing eu
-                        reprehenderit enim.
+                    In this project we used firebase mainly for two reasons. These are authentication and hosting. Firebase are popular for authentication purposes. By using firebase, we easily implement email/password login as well as third party one-click login. Besides that, our project is hosted on the firebase. There are many firebase alternative we can use such as oauth, supabase, MongoDB etc
                     </p>
                 </AccordionItemPanel>
             </AccordionItem>
             <AccordionItem>
                 <AccordionItemHeading>
                     <AccordionItemButton>
-                        What harsh truths do you prefer to ignore?
+                    How does the private route work?
                     </AccordionItemButton>
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                        Exercitation in fugiat est ut ad ea cupidatat ut in
-                        cupidatat occaecat ut occaecat consequat est minim minim
-                        esse tempor laborum consequat esse adipisicing eu
-                        reprehenderit enim.
+                    Private route works very simply. If we set a route inside a private route then we try to access that route it will first go to that private route. In that private route there will be some condition depending on that it may go to that route or to a route that redirects from private route.  
                     </p>
                 </AccordionItemPanel>
             </AccordionItem>
             <AccordionItem>
                 <AccordionItemHeading>
                     <AccordionItemButton>
-                        Is free will real or just an illusion?
+                    What is Node? How does Node work?
                     </AccordionItemButton>
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                        In ad velit in ex nostrud dolore cupidatat consectetur
-                        ea in ut nostrud velit in irure cillum tempor laboris
-                        sed adipisicing eu esse duis nulla non.
+                    In one word, Node is JavaScript runtime that is built on Chrome’s V8 engine. We all know that JavaScript one works with-thin browser. But Rayn-dahl make it possible to run JavaScript outside the browser also. That’s why we don’t need separate language for server-side application. JavaScript is non-blocking I/O meaning that one request needn’t to wait for another one to complete first.
                     </p>
                 </AccordionItemPanel>
             </AccordionItem>
